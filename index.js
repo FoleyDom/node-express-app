2d9c4c80a705d5a8960806db5ea278f5916656d1
FoleyDom <dfoleydev@gmail.com>
2022-06-09 22:31:41 -0400

    Not working, changes to get/api unknown error

--- index.js
+++ index.js
@@ -2,32 +2,44 @@ const express = require('express')
 const app = express()
 
 let notes = [
-    {
-        id: 1,
-        content: 'HTML is easy',
-        date: '2022-05-30T17:30:31.098Z',
-        important: true,
-    },
-    {
-        id: 2,
-        content: 'Browser can execute only Javascript',
-        date: '2022-05-30T18:39:34.091Z',
-        important: false,
-    },
-    {
-        id: 3,
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        date: '2022-05-30T19:20:14.298Z',
-        important: true,
-    },
+	{
+		id: 1,
+		content: 'HTML is easy',
+		date: '2022-05-30T17:30:31.098Z',
+		important: true,
+	},
+	{
+		id: 2,
+		content: 'Browser can execute only Javascript',
+		date: '2022-05-30T18:39:34.091Z',
+		important: false,
+	},
+	{
+		id: 3,
+		content: 'GET and POST are the most important methods of HTTP protocol',
+		date: '2022-05-30T19:20:14.298Z',
+		important: true,
+	},
 ]
 
 app.get('/', (request, response) => {
-    response.send(`<h1>Hello World</h1>`)
+	response.send(`<h1>Hello World</h1>`)
 })
 
-app.get('/api/notes', (request, response) => {
-    response.json(notes)
+app.get('/api/notes/:id', (request, response) => {
+	const id = Number(request.params.id)
+	console.log(id)
+	const note = notes.find((note) => {
+		// console.log(note.id, typeof note.id, id, typeof id, note.id === id)
+		return note.id === id
+	})
+	if (note) {
+		response.json(note)
+	} else {
+		response.status(404).end()
+	}
+	console.log(note)
+	response.json(notes)
 })
 
 const PORT = 3001
