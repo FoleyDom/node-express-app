e14e1a3f3b29366cc568dad66261a1d9df6d805a
FoleyDom <dfoleydev@gmail.com>
2022-06-12 19:26:26 -0400

    Finsihed with error for missing content and generating a new id number

--- index.js
+++ index.js
@@ -47,8 +47,29 @@ app.delete('/api/notes/:id', (request, response) => {
 	response.status(204).end()
 })
 
+const generateId = () => {
+	const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0
+	return maxId + 1
+}
+
 app.post('/api/notes', (request, response) => {
-	const note = request.body
+	const body = request.body
+
+	if (!body.content) {
+		return response.status(400).json({
+			error: 'content missing',
+		})
+	}
+
+	const note = {
+		content: body.content,
+		import: body.import || false,
+		date: new Date(),
+		id: generateId(),
+	}
+
+	notes = notes.concat(note)
+
 	console.log(note)
 	response.json(note)
 })
